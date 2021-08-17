b2fa09fe7e60eace0e4917675a6c8d176f5100a6
Hlöðver Sigurðsson <hlolli@gmail.com>
2021-08-17 21:11:00 +0200

    add vartex_git_revision to /status

--- src/route/status.route.ts
+++ src/route/status.route.ts
@@ -8,8 +8,14 @@ import {
 } from "../database/sync.database.js";
 import { toLong } from "../database/cassandra.database.js";
 import { getNodeInfo } from "../query/node.query.js";
+import gitRev from "git-rev-sync";
 
 export const start = Date.now();
+let gitRevision = "unknown";
+
+try {
+  gitRevision = gitRev.long([process.cwd()]);
+} catch {}
 
 export async function statusRoute(request: Request, response: Response) {
   try {
@@ -27,6 +33,7 @@ export async function statusRoute(request: Request, response: Response) {
         queuedTxs: getTxQueueSize(),
         queuedBlocks: getBlockQueueSize(),
       },
+      vartex_git_revision: gitRevision,
     });
   } catch (error) {
     return response.status(503).send(error);
